05f0a6ac2e9a6af3de0224a803bc18f2075daa0d
Brunom8218 <bmars8218@gmail.com>
2023-11-05 03:58:15 +1300

    231104.0 complete the search result according to Hospital and Practice

--- client/find-patient/index.js
+++ client/find-patient/index.js
@@ -1,119 +1,156 @@
-import './findPatient.html'
+import "./findPatient.html"
 
-import { Template } from 'meteor/templating'
+import { Template } from "meteor/templating"
 import { Meteor } from "meteor/meteor"
-import { ReactiveVar } from 'meteor/reactive-var';
-import { Session } from 'meteor/session';
-
-
+import { ReactiveVar } from "meteor/reactive-var"
+import { Session } from "meteor/session"
+// import { Tracker } from "meteor/tracker"
 
 Template.findPatient.onCreated(function loginOnCreated() {
-    this.searchLastName = new ReactiveVar("");
-    this.searchFirstName = new ReactiveVar("");
-    this.searchDate = new ReactiveVar("");
-    this.searchEncounter = new ReactiveVar("");
-    this.headers = new ReactiveVar("");
-    this.findPatientHos = new ReactiveVar("");
-    this.findPatientPra = new ReactiveVar(""); 
-    this.isFindLoading = new ReactiveVar(false);
-  });
-  
-  Template.findPatient.helpers({
-    headers() {
-        return Session.get("headers")
-    },
-    findPatientHos() {
-        return Template.instance().findPatientHos.get();
-    },
-    findPatientPra() {
-        return Template.instance().findPatientPra.get();
-    },
-    isFindLoading() {
-        return Template.instance().isFindLoading.get();
+	this.searchLastName = new ReactiveVar("")
+	this.searchFirstName = new ReactiveVar("")
+	this.searchDate = new ReactiveVar("")
+	this.searchEncounter = new ReactiveVar("")
+	this.headers = new ReactiveVar("")
+	this.resultPatients = new ReactiveVar("")
+	this.isFindLoading = new ReactiveVar(false)
+    // this.isActive = new ReactiveVar("")
+    
+})
+
+Template.findPatient.helpers({
+	headers() {
+		return Session.get("headers")
+	},
+	findPatientHos() {
+		return Session.get("findPatientHos")?.patients
+	},
+	findPatientPra() {
+		return Session.get("findPatientPra")?.patients
+	},
+    // resultPatients() {
+    //     return Template.instance().resultPatients.get();
+    // },
+	isFindLoading() {
+		return Template.instance().isFindLoading.get()
+	},
+    isActive() {
+        return Session.get("isActive") === "hospital";
     }
-  });
-  
-  Template.findPatient.events({
-    async 'submit .search-patient-form'(event, instance) {
-        event.preventDefault();
-        const target = event.target;
-        const lastName = target.lastName.value.toLowerCase();
-        const firstName = target.firstName.value.toLowerCase();
-        
+})
 
-        if (!(lastName || firstName )) {
+// Tracker.autorun(() => {
+    
+  
+//     if (Template.instance().findPatientHos.get()) {
+//     //   Session.set('oldest', oldest.name);
+        
+//     }
+//   });
+
+Template.findPatient.events({
+	async "submit .search-patient-form"(event, instance) {
+		event.preventDefault()
+		const target = event.target
+		const lastName = target.lastName.value.toLowerCase()
+		const firstName = target.firstName.value.toLowerCase()
+
+		if (!(lastName || firstName)) {
+			return
+		}
+
+		instance.isFindLoading.set(true)
+		const isActive = Session.get("isActive")
+		const facility = Session.get("facilities")[0]
+		const practice = Session.get("practices")[0]
+		const authToken = Session.get("headers")
+		const coreUrl = () => {
+			if (isActive === "hospital") {
+				return facility.systems[0].coreUrl
+			} else {
+				return practice.systems[0].coreUrl
+			}
+		}
+
+		const buildQuery = () => {
+			if (lastName && firstName) {
+				return `Patient?family=${lastName}&given=${firstName}`
+			} else {
+				return `Patient?family=${lastName}`
+			}
+		}
+
+		const getFindPatients = async (coreUrl, query, headers) => {
+			return new Promise(function (resolver, reject) {
+				Meteor.call(
+					"patientTestQuery",
+					`${coreUrl}/${query}`,
+					headers,
+					(error, result) => {
+						if (error) {
+							console.log("errorFinding", error)
+							reject(error)
+						} else {
+							if (result.status === 200) {
+								resolver(result)
+							}
+						}
+					}
+				)
+			}).catch((error) => {
+				// show error on screen
+                Session.set("findPatientPra", null)
+				instance.isFindLoading.set(false)
+				// alert("Error: " + "There is no Search Result")
+                
+			})
+		}
+		const res = await getFindPatients(coreUrl(), buildQuery(), {
+			Authorization: authToken,
+		})
+		console.log("res", res)
+        
+		instance.isFindLoading.set(false)
+		if (isActive === "hospital") {
+            if (res) {
+                Session.set("findPatientHos", {
+                    patients: res.bundle.entry,
+                    cache: {
+                        id: res.queryId,
+                        pageNumber: res.pageNumber,
+                        totalPages: res.pageNumber,
+                        countInPage: res.countInPage,
+                    },
+                })
+            } else {
+                Session.set("findPatientHos", null)
+            }
+		}  else {
             
-            return
-        }
-        
-        instance.isFindLoading.set(true);
-        const isActive = Session.get("isActive")
-        const facility = Session.get("facilities")[0]
-        const practice = Session.get("practices")[0]
-        const authToken = Session.get("headers")
-        const coreUrl = () => {
-            if (isActive === "hospital") {
-                return facility.systems[0].coreUrl;
+            if (res) {
+            Session.set("findPatientPra", {
+                patients: res.bundle.entry,
+                cache: {
+                    id: res.queryId,
+    
+                    pageNumber: res.pageNumber,
+                    totalPages: res.pageNumber,
+                    countInPage: res.countInPage,
+                },
+            })
             } else {
-                return practice.systems[0].coreUrl;
+                Session.set("findPatientPra", null)
             }
-        }
+		}
         
-        const buildQuery = () => {
 
-            if (lastName && firstName) {
-                return `Patient?family=${lastName}&given=${firstName}`
-            } else {
-                return `Patient?family=${lastName}`
-            }
-        }
-        const findPatientBody = {
-            facility: facility.id,
-            fhirSystem: facility.systems[0].id,
-            fhirAuthToken: authToken.split(" ")[1]
-        }
+		return false
+	},
+	"change #inputState": function (event, data) {
+		// event.target.value contains selected value
+		console.log("Selection changed to: " + event.target.value)
 
-        const getFindPatients = async (coreUrl, query, headers) => {
-            return new Promise(function (resolver, reject) {
-                Meteor.call(
-                    "patientTestQuery",
-                    `${coreUrl}/${query}`,
-                    headers,
-                    (error, result) => {
-                        if (error) {
-                            console.log("errorFinding", error);
-                            reject(error)
-                        } else {
-                            if (result.status === 200) {
-                                
-                                resolver(result)
-                            }
-                        }
-                    }
-                );
-            });
-        };
-        const res = await getFindPatients(coreUrl(), buildQuery(), {Authorization: authToken})
-        console.log("res", res)
-        instance.isFindLoading.set(false);
-        Session.set("findPatientHos", {
-            patients: res.bundle.entry,
-            cache: {
-                id: res.queryId,
-                pageNumber: res.pageNumber,
-                totalPages: res.pageNumber,
-                countInPage: res.countInPage,
-            }
-        });
-        // const patients = res.bundle.entry.map((e) => (
-        //     e.text.div.replace('div', 'div style=\'display: flex;\'')
-        // ))
-        // instance.findPatientHos.set(patients);
-        instance.findPatientHos.set(res.bundle.entry);
-        console.log("patients--", instance.findPatientHos.get());
-        
-        return false;
-    },
-  });
-  
-  
+		console.log("Selection changed to: " + data)
+		// handle change here
+	},
+})
